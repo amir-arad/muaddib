552576b457979c0bd3d0b15f95cc2f72d5391493
amir-arad <greenshade@gmail.com>
2018-06-06 18:55:35 +0300

    more setup of system module - add testkit, remove extra typings

--- system/test/setup.ts
+++ system/test/setup.ts
@@ -1,6 +1,4 @@
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
-import * as chaiSubset from 'chai-subset';
 
 chai.use(sinonChai);
-chai.use(chaiSubset);
--- system/test/stage1.spec.ts
+++ system/test/stage1.spec.ts
@@ -1,13 +1,15 @@
-import {expect} from 'chai';
-import * as sinon from 'sinon';
+import {plan} from "./testkit/chai.spec";
 
 
+describe('system', () => {
+    describe('stage 1 - single vm, shallow plugins demo app', () => {
 
+        it('load two plugins and have them all find and communicate with each other', plan(1, () => {
 
-describe('system - stage1', () => {
-    // simple unit test
-    it('greets', ()=>{
+        }));
 
+        xit('plugins as prototype and singleton', plan(1, () => {
+
+        }));
     });
-
 });
