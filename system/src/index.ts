00c26b0b496c47afb39a2d6ddd7c95a39d1bbbda
amir-arad <greenshade@gmail.com>
2018-06-25 16:32:24 +0300

    tiny reorder of lines

--- system/src/index.ts
+++ system/src/index.ts
@@ -2,12 +2,12 @@ import {System} from "./types";
 import {SystemImpl} from "./system";
 import {Container} from "./dependencies";
 
-export * from './types'
-export {isMessageType, LinkMessage, SystemMessage} from './network'
-export * from './dependencies'
-
 let counter = 0;
 
 export function createSystem<D>(name: string = 'System:' + (counter++), container: Container<D> = new Container()): System<D> {
     return new SystemImpl<D>(name, container);
 }
+
+export * from './types'
+export {isMessageType, LinkMessage, SystemMessage} from './network'
+export * from './dependencies'
